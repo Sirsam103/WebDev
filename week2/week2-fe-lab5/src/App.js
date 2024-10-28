c5a167dcf8e799148f2ca1e5653f96649ed93de3
Sirsam <samuelpuhakka@gmail.com>
2024-10-28 12:23:54 +0200

    week2 monday labs

---
+++ week2/week2-fe-lab5/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+import './styles.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
